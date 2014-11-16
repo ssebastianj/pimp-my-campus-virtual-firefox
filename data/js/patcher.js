0c2fcb2e21cbfb1718de4c9d2c21d8d20a3c0597
Sebastián J. Seba <ssebastianj@gmail.com>
2014-11-16 13:35:54 -0300

    Eliminadas líneas de depuración (console.log). Unificados strings de comillas dobles a comillas simples.
    Signed-off-by: Sebastián J. Seba <ssebastianj@gmail.com>

--- data/js/patcher.js
+++ data/js/patcher.js
@@ -1,14 +1,11 @@
-console.log('patcher.js loaded');
-
-self.port.on("detach", function() {
-  'use strict';
-  console.log('patcher.js detached');
-});
-
 var formularioBusqueda = document.getElementById('coursesearch');
 var divInst3 = document.getElementById('inst3');
 var parentDiv = divInst3.parentNode;
-var divForm = document.createElement("div");
+var divForm = document.createElement('div');
+
+self.port.on('detach', function() {
+  'use strict';
+});
 
 document.getElementById('shortsearchbox').size = 20;
 divForm.id = 'search-form';

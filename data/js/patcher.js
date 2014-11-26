3d0f4f1790e4bbe40003aabeb3029baa8e174f2b
Sebastián J. Seba <ssebastianj@gmail.com>
2014-11-26 17:23:54 -0300

    Agregada regla CSS para solucionar legibilidad de fuente en elementos 'input'. - Refactoring de patcher JS (mayor encapsulación). - Versión: 0.2
    Signed-off-by: Sebastián J. Seba <ssebastianj@gmail.com>

--- data/js/patcher.js
+++ data/js/patcher.js
@@ -1,13 +1,17 @@
-var formularioBusqueda = document.getElementById('coursesearch');
-var divInst3 = document.getElementById('inst3');
-var parentDiv = divInst3.parentNode;
-var divForm = document.createElement('div');
+function relocateSearchBox(doc) {
+  var formularioBusqueda = doc.getElementById('coursesearch');
+  var divInst3 = doc.getElementById('inst3');
+  var parentDiv = divInst3.parentNode;
+  var divForm = doc.createElement('div');
 
-self.port.on('detach', function() {
-  'use strict';
-});
+  self.port.on('detach', function() {
+    'use strict';
+  });
 
-document.getElementById('shortsearchbox').size = 20;
-divForm.id = 'search-form';
-divForm.appendChild(formularioBusqueda);
-parentDiv.insertBefore(divForm, divInst3);
+  doc.getElementById('shortsearchbox').size = 20;
+  divForm.id = 'search-form';
+  divForm.appendChild(formularioBusqueda);
+  parentDiv.insertBefore(divForm, divInst3);
+}
+
+relocateSearchBox(document);
